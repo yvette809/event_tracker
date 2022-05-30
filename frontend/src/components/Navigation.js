3840bf4d1e033d19d59cbce2faccdf8fc064cdb6
Yvette <nchombuayvta@gmil.com>
2022-05-30 15:42:05 +0200

    styling

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -26,25 +26,10 @@ const Navigation = ({ setShowModal }) => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light ">
 
-        <div className="container-fluid">
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-mdb-toggle="collapse"
-            data-mdb-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i className="fas fa-bars"></i>
-          </button>
-
-          <div className="collapse navbar-collapse d-flex align-items-center" id="navbarSupportedContent">
-            <Link to="/" className='text-white'><h2><span className="text-info">E-</span>Tra<span className="text-info">c</span>ker</h2></Link>
+        <div className="container-fluid nav-container">
+          <div className="collapse navbar-collapse d-flex align-items-center">
+            <Link to="/" className='text-white '><h2><span className="text-info">E-</span>Tra<span className="text-info">c</span>ker</h2></Link>
           </div>
-
-
 
           <div className="d-flex align-items-center">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
@@ -52,15 +37,11 @@ const Navigation = ({ setShowModal }) => {
               <li className="nav-link add-event" onClick={() => setShowModal(true)}>
                 Add New Event
               </li>
-              <Link to="/pass-events">
-                <li className="nav-link" >
-                  Pass Events
-                </li>
-              </Link>
+
             </ul>
 
 
-            <div className="dropdown">
+            <div className="dropdown ">
               <Link
                 className="dropdown-toggle d-flex align-items-center hidden-arrow"
                 to="/"
@@ -83,24 +64,35 @@ const Navigation = ({ setShowModal }) => {
               {userInfo.token ? (
 
                 <ul
-                  className="dropdown-menu dropdown-menu-end"
+                  className="dropdown-menu dropdown-menu-end items-dropdown"
                   aria-labelledby="navbarDropdownMenuAvatar"
                 >
                   <li>
                     <Link className="dropdown-item" to="/">{name && name}</Link>
-                  </li>
+                  </li><hr />
                   <li>
                     <Link className="dropdown-item " to="/">{email && email}</Link><hr />
                   </li>
                   <li>
+                    <Link className="dropdown-item" to="/pass-events" >
+                      Pass Events
+                    </Link>
+                  </li><hr />
+                  <li>
                     <Link className="dropdown-item" to="/login" onClick={logUserOut}>Logout</Link>
                   </li>
+
                 </ul>
               ) : (
                 <ul
                   className="dropdown-menu dropdown-menu-end"
                   aria-labelledby="navbarDropdownMenuAvatar"
                 >
+                  <li>
+                    <Link className="dropdown-item" to="/pass-events" >
+                      Pass Events
+                    </Link>
+                  </li><hr />
                   <li>
                     <Link className="dropdown-item " to="/login">Login</Link>
                   </li>
